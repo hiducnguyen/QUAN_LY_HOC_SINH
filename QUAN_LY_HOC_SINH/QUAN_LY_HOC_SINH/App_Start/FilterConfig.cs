64029a8d2888e24485ab4a220170d91b170e1ffc
hellovanduc <nguyenduc21022k@gmail.com>
2021-05-19 09:30:13 +0700

    Init Solution

---
+++ QUAN_LY_HOC_SINH/QUAN_LY_HOC_SINH/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace QUAN_LY_HOC_SINH
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ QUAN_LY_HOC_SINH/QUAN_LY_HOC_SINH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QUAN_LY_HOC_SINH.MvcApplication" Language="C#" %>
