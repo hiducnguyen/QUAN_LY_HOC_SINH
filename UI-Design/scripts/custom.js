3529e30c7d90ec9a0117c99654bc56fa85dd7e44
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-20 09:30:41 +0700

    Finish UI version 1.0

--- UI-Design/scripts/custom.js
+++ UI-Design/scripts/custom.js
@@ -1,31 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    //  Apply multi-select framework for the multi-select box
     $('.multi_select').select2();
+    
+    //  Apply datatable framework for the tables
     $('.data_table').DataTable({
         "language": {
-            "decimal":        "",
-            "info":           "Hiển thị từ _START_ tới _END_ trong _TOTAL_ hàng",
-            "infoEmpty":      "Hiển thị từ 0 tới 0 trong 0 hàng",
-            "infoPostFix":    "",
-            "thousands":      ",",
-            "lengthMenu":     "Hiển thị _MENU_ hàng",
+            "decimal": "",
+            "info": "Hiển thị từ _START_ tới _END_ trong _TOTAL_ hàng",
+            "infoEmpty": "Hiển thị từ 0 tới 0 trong 0 hàng",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Hiển thị _MENU_ hàng",
             "loadingRecords": "Đang tải...",
-            "processing":     "Đang xử lý...",
-            "search":         "Tìm kiếm:",
-            "zeroRecords":    "Không tìm thấy kết quả nào",
+            "processing": "Đang xử lý...",
+            "search": "Tìm kiếm:",
+            "zeroRecords": "Không tìm thấy kết quả nào",
             "paginate": {
-                "first":      "Đầu tiên",
-                "last":       "Cuối cùng",
-                "next":       "Sau",
-                "previous":   "Trước"
+                "first": "Đầu tiên",
+                "last": "Cuối cùng",
+                "next": "Sau",
+                "previous": "Trước"
             }
-        },
-        buttons: [
-            {
-                text: 'My button',
-                action: function ( e, dt, node, config ) {
-                    alert( 'Button activated' );
-                }
-            }
-        ]
+        }
+    });
+
+    //  Triggered when deleteModal is about to be shown
+    $('#deleteModal').on('show.bs.modal', (e) => {
+
+        //  Get data-id attribute of the clicked element
+        let id = $(e.relatedTarget).data('id');
+
+        //  Populate the text box
+        document.querySelector('#deleteModal input[name="id"]').innerText = id;
+        document.querySelector('#deleteModal #id_to_delete').innerText = id;
     });
 });
