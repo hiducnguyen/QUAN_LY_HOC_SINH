bfbd014518b761bb2e080679594fb61a9c4058f2
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-21 21:52:03 +0700

    Create Business Exception ver1.0

---
+++ QUAN_LY_HOC_SINH/Services/Exceptions/StudentIdAlreadyExistsException.cs
@@ -0,0 +1,18 @@
+﻿using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Exceptions
+{
+    public class StudentIdAlreadyExistsException : BaseException
+    {
+        public StudentIdAlreadyExistsException(int studentId) 
+            : base(string.Format(Resource.StudentIdAlreadyExists, studentId))
+        {
+
+        }
+    }
+}
---
+++ QUAN_LY_HOC_SINH/Services/Exceptions/StudentNotExistsException.cs
@@ -0,0 +1,18 @@
+﻿using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Exceptions
+{
+    public class StudentNotExistsException : BaseException
+    {
+        public StudentNotExistsException(int studentId) :
+            base(string.Format(Resource.StudentDoesNotExists, studentId))
+        {
+
+        }
+    }
+}
