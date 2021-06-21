c18273d40038dcf769a11392603802c9a345f0ce
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-21 21:41:36 +0700

    Base Controller to handle Exception

---
+++ QUAN_LY_HOC_SINH/QUAN_LY_HOC_SINH/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QUAN_LY_HOC_SINH.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception e = filterContext.Exception;
+
+            ViewData["ErrorMessage"] = e.Message;
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "Error",
+                ViewData = this.ViewData
+            };
+
+            filterContext.ExceptionHandled = true;
+            base.OnException(filterContext);
+        }
+    }
+}
