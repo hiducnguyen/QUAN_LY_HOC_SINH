aaa8943be2494cdb0cb8e827d0848f53dd8b8a54
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-21 21:51:13 +0700

    Create IndexStudentDTO.cs

---
+++ QUAN_LY_HOC_SINH/Services/DTO/IndexStudentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO
+{
+    public class IndexStudentDTO
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
