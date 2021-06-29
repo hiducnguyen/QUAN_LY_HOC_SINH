ac4c4ec8bc23245f9c4e8d4c45f1065f2e6da667
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-29 22:00:35 +0700

    Implement TranscriptRepository

---
+++ QUAN_LY_HOC_SINH/Repositories/ITranscriptRepository.cs
@@ -0,0 +1,41 @@
+﻿using Repositories.Enums;
+using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface ITranscriptRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IList<Transcript> FindAllTranscripts();
+        /// <summary>
+        /// Find all class and corresponding transcript of each semester
+        /// </summary>
+        /// <returns></returns>
+        IList<TranscriptOfClass> FindAllTranscriptsOfClass();
+        /// <summary>
+        /// Find class and corresponding transcript of semester
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="subjectId"></param>
+        /// <param name="Semester"></param>
+        /// <returns></returns>
+        TranscriptOfClass FindTranscriptsOfClass(string className, int subjectId, Semester Semester);
+
+        /// <summary>
+        /// Find transcript of student in corresponding subject and semester
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="studentId"></param>
+        /// <param name="semester"></param>
+        /// <returns></returns>
+        Transcript FindTranscript(Subject subject, Guid studentId, Semester semester);
+    }
+}
---
+++ QUAN_LY_HOC_SINH/Repositories/TranscriptRepository.cs
@@ -0,0 +1,48 @@
+﻿using Repositories.Enums;
+using Repositories.Models;
+using Repositories.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class TranscriptRepository : ITranscriptRepository
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public TranscriptRepository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IList<Transcript> FindAllTranscripts()
+        {
+            throw new NotImplementedException();
+        }
+        public IList<TranscriptOfClass> FindAllTranscriptsOfClass()
+        {
+            return _unitOfWork.Session.QueryOver<TranscriptOfClass>().List();
+        }
+
+        public Transcript FindTranscript(Subject subject, Guid studentId, Semester semester)
+        {
+            return _unitOfWork.Session.QueryOver<Transcript>()
+                .Where(x => x.Subject == subject &&
+                    x.StudentId == studentId &&
+                    x.Semester == semester)
+                .SingleOrDefault();
+        }
+
+        public TranscriptOfClass FindTranscriptsOfClass(string className, int subjectId, Semester Semester)
+        {
+            return _unitOfWork.Session.QueryOver<TranscriptOfClass>()
+                .Where(x => x.ClassName == className &&
+                    x.SubjectId == subjectId &&
+                    x.Semester == Semester)
+                .SingleOrDefault();
+        }
+    }
+}
