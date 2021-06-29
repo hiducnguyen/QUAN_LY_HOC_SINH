0d8ad31b23521bd48d43884f1893bff610c1424a
hellovanduc <nguyenduc21022k@gmail.com>
2021-06-29 22:01:34 +0700

    Implement TranscriptController and View

---
+++ QUAN_LY_HOC_SINH/QUAN_LY_HOC_SINH/Controllers/TranscriptController.cs
@@ -0,0 +1,124 @@
+﻿using Repositories.Enums;
+using Resources;
+using Services;
+using Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace QUAN_LY_HOC_SINH.Controllers
+{
+    public class TranscriptController : BaseController
+    {
+        private ITranscriptService _transcriptService;
+        private IClassService _classService;
+        private ISubjectService _subjectService;
+        public TranscriptController(ITranscriptService scriptService,
+            IClassService classService,
+            ISubjectService subjectService)
+        {
+            _transcriptService = scriptService;
+            _classService = classService;
+            _subjectService = subjectService;
+        }
+
+        // GET: Transcript
+        public ActionResult Index()
+        {
+            IList<IndexTranscriptDTO> model = _transcriptService.FindAllTranscripts();
+            ViewBag.Title = Resource.ListScripts;
+            return View(model);
+        }
+
+        // GET: Transcript/Create
+        public ActionResult Create()
+        {
+            CreateTranscriptDTO model = new CreateTranscriptDTO
+            {
+                AllClasses = _classService.GetSelectListClasses(),
+                AllSemesters = SemesterHelper.GetAllSemesters(),
+                AllSubjects = _subjectService.GetSelectListSubjects()
+            };
+            ViewBag.Title = Resource.CreateTranscript;
+            return View(model);
+        }
+
+        // POST: Transcript/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ClassName,SubjectId,Semester")]
+            CreateTranscriptDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                _transcriptService.CreateTranscript(model);
+                return RedirectToAction("Detail", new RouteValueDictionary {
+                    {"subjectId", model.SubjectId },
+                    {"className", model.ClassName },
+                    {"semester", model.Semester }
+                });
+            }
+            else
+            {
+                ViewBag.Title = Resource.CreateTranscript;
+                model.AllClasses = _classService.GetSelectListClasses();
+                model.AllSemesters = SemesterHelper.GetAllSemesters();
+                model.AllSubjects = _subjectService.GetSelectListSubjects();
+                return View(model);
+            }
+        }
+
+        [Route("transcript/detail/{subjectId}/{className}/{semester}")]
+        public ActionResult Detail(int subjectId, string className, int semester)
+        {
+            IList<TranscriptDetailDTO> model = _transcriptService.FindTranscripts(subjectId, className, semester);
+
+            ViewBag.ClassName = className;
+            ViewBag.SubjectName = _subjectService.FindSubjectBySubjectId(subjectId).Name;
+            ViewBag.Semester = semester;
+            ViewBag.Title = Resource.TranscriptDetail;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("transcript/detail/{subjectId}/{className}/{semester}")]
+        public ActionResult Detail(int subjectId, string className, int semester,
+            [Bind(Include = "StudentId,FifteenMinutesTestScore,FortyFiveMinutesTestScore,FinalTestScore,Version")]
+            IList<TranscriptDetailDTO> model)
+        {
+            if (ModelState.IsValid)
+            {
+                _transcriptService.UpdateTranscripts(subjectId, className, semester, model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ClassName = className;
+                ViewBag.SubjectName = _subjectService.FindSubjectBySubjectId(subjectId).Name;
+                ViewBag.Semester = semester;
+                ViewBag.Title = Resource.TranscriptDetail;
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            // format of id: "{SubjectId}/{ClassName}/{Semester}"
+            if (id != null)
+            {
+                string[] ids = id.Split('/');
+                if (ids.Length == 3)
+                {
+                    _transcriptService.DeleteTranscripts(Convert.ToInt32(ids[0]), ids[1], Convert.ToInt32(ids[2]));
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- QUAN_LY_HOC_SINH/Repositories/Enums/Semester.cs
+++ QUAN_LY_HOC_SINH/Repositories/Enums/Semester.cs
@@ -1,8 +1,28 @@
-﻿namespace Repositories.Enums
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Repositories.Enums
 {
     public enum Semester
     {
         First = 1,
         Second = 2
+    }
+    public class SemesterHelper
+    {
+        public static SelectList GetAllSemesters()
+        {
+            IEnumerable<SelectListItem> allSemesters = new List<SelectListItem>(Enumerable.Range(1, 2)
+                .Select(x => new SelectListItem
+                {
+                    Value = x.ToString(),
+                    Text = x.ToString()
+                })
+            );
+
+            return new SelectList(allSemesters.OrderBy(x => x.Text), "Value", "Text");
+        }
     }
 }
